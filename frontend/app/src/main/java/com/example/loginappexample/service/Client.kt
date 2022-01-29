f3de7ca1f7d8634ccfcae9fbe2c74cd0c6561a9f
maciekdt <maciek.fi88@gmail.com>
2022-01-29 18:51:34 +0100

    added JSON serialization by gson

--- frontend/app/src/main/java/com/example/loginappexample/service/Client.kt
+++ frontend/app/src/main/java/com/example/loginappexample/service/Client.kt
@@ -4,27 +4,47 @@ import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.*
 import io.ktor.client.features.get
+import io.ktor.client.features.json.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import java.util.*
 
 class Client {
+
+    private val subdomain = "https://maciekdt.loca.lt"
+
+    private val client = HttpClient(CIO){
+        install(JsonFeature) {
+            serializer = GsonSerializer()
+        }
+        expectSuccess = false
+    }
+
+
     @RequiresApi(Build.VERSION_CODES.O)
     suspend fun loginRequest(name:String, password:String){
-        val auth = name+":"+password
-        val encodedAuth: String = Base64.getEncoder().encodeToString(auth.toByteArray())
-        val client = HttpClient()
-        val response: HttpResponse = client.get("https://maciekdt.loca.lt/api/users/"){
+        val route = "/api/users/"
+        val response:HttpResponse = client.get(subdomain + route){
+            method = HttpMethod.Get
             headers {
-                append(HttpHeaders.Authorization, "Basic " + encodedAuth)
+                append(HttpHeaders.Authorization, encodeAuthHeader(name, password))
             }
         }
-
+        val data:LoginResponseData = response.receive()
         Log.i("MyInfo", "Response status : " + response.status.toString())
-        client.close()
+        Log.i("MyInfo", "Response body : " + data.toString())
+        //client.close()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun encodeAuthHeader(name:String, password:String):String {
+        val auth = "$name:$password"
+        val encodedAuth:String = Base64.getEncoder().encodeToString(auth.toByteArray())
+        return ("Basic $encodedAuth")
     }
 }
